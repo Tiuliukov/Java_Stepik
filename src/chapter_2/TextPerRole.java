8887bbc9e57f405754ecd8f589dc5e4d9ffcb109
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2022-08-02 21:58:26 +0300

    Add task 2.4.3

---
+++ src/chapter_2/TextPerRole.java
@@ -0,0 +1,30 @@
+package chapter_2;
+
+public class TextPerRole {
+    String [] roles= {
+            "Городничий","Аммос Федорович",
+            "Артемий Филиппович",
+            "Лука Лукич"};
+    String [] textLines={"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+        "Аммос Федорович: Как ревизор?",
+        "Артемий Филиппович: Как ревизор?",
+        "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+        "Аммос Федорович: Вот те на!",
+        "Артемий Филиппович: Вот не было заботы, так подай!",
+        "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+
+    private String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder answer = new StringBuilder();
+        for (String role : roles) {
+            answer.append(role + ":\n");
+            for (int i = 0; i < textLines.length; i++) {
+                if (textLines[i].startsWith(role + ":")) {
+                    answer.append((i+1) + ")" + textLines[i].substring(role.length() + 1) + "\n");
+                }
+            }
+            answer.append("\n");
+        }
+        return answer.toString();
+    }
+
+}
